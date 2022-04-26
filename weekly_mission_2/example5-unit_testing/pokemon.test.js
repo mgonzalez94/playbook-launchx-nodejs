efb53e9b865eccd984a04c578183215fff9fa17f
MGonzalez <1994.magr@gmail.com>
2022-04-25 21:22:55 -0500

    fix: test_suit pokemon gengar

--- weekly_mission_2/example5-unit_testing/pokemon.test.js
+++ weekly_mission_2/example5-unit_testing/pokemon.test.js
@@ -1,6 +1,6 @@
 import Pokemon from "./pokemon";
 
 test('1) Create a new object Pokemon', () => {
-    const myPokemon = new Pokemon('Pikachu')
-    expect(myPokemon.name).toBe('Pikachussssss'); //Corrige esta prueba
+    const myPokemon = new Pokemon('Gengar')
+    expect(myPokemon.name).toBe('Gengar'); //Corrige esta prueba
 });
